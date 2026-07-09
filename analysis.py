0173bf7f84db39c0b1d670aaef55e0ebcad400a9
Sandeep Kumar <krsandeep8507@gmail.com>
2026-07-09 12:22:46 +0530

    Implemented baseline Random Forest model

--- analysis.py
+++ analysis.py
@@ -103,3 +103,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("Train Shape:", X_train.shape)
 print("Test Shape:", X_test.shape)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+model = RandomForestClassifier(random_state=42)
+
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, pred))
